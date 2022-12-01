c141bd7a8f14cfaa5146691d52318933566ec72a
lisevsky <hi.lisevsky@ya.ru>
2022-12-01 14:30:36 +0300

    remove auth pages

--- src/pages/Auth/MainPage/index.tsx
+++
@@ -1,8 +0,0 @@
-import React from 'react';
-
-export const MainPage = () => (
-  <div style={{'background': 'gold', 'width': 400, 'height': 200, 
-    'display': 'flex', 'alignItems': 'center', 'justifyContent': 'center'}}>
-    Beaty from NASA for YOU
-  </div>
-);
